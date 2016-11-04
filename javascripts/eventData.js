1eab049065e6ac034b81694d04ed7ac1f8fb0ae9
MichaelSifuentes <michaela.sifuentes@yahoo.com>
2016-11-03 19:14:48 -0700

    Add files via upload

---
+++ javascripts/eventData.js
@@ -0,0 +1,34 @@
+var events = [
+  {
+    title: 'Hack Poly 2017',
+    date: '02/11/17 10:00AM',
+    description: 'Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit,  \
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo     \
+      consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis   \
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    links: [
+      {
+        name: 'Website',
+        url: 'http://hackpoly.com/'
+      }
+    ],
+  },
+  {
+    title: 'Speaker',
+    date: '06/23/16 5:00PM',
+    description: 'Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit,  \
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo     \
+      consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis   \
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    links: [
+      {
+        name: 'SDLC',
+        url: 'https://aws.amazon.com/'
+      }
+    ]
+  }
+];
---
+++ javascripts/workshopData.js
@@ -0,0 +1,54 @@
+var workshops = [
+  {
+    title: 'Intro to Programming using Javascript Workshop',
+    date: '11/01/16 5:00PM',
+    description: 'Description: In this workshop we went over basic programming syntax for \
+	for Javascript to help prepare for a future workshop where we can use a more hands on \
+	approach and show how javascript can be used for web development. We went over a      \
+	powerpoint and did some examples using the google chrome console. Download the        \
+	powerpoint if you need a refresher on programming or javascript syntax.',
+    links: [
+      {
+        name: 'Tutorial',
+        url: 'https://www.codecademy.com/learn/javascript'
+      },
+      {
+        name: 'Javascript',
+        url: 'http://www.w3schools.com/js/'
+      }
+    ],
+    files: [
+      'javascript.ppt',
+	  'javascript.txt'
+    ]
+  },
+  {
+    title: 'HTML/CSS Workshop',
+    date: '10/28/16 5:00PM',
+    description: 'Description: Started the workshop going over websites where anyone can   \
+	learn web development at home, such as codeacademy.com, Lyndia.com, and freecodecamp.com. \
+	We also went over some text-editors that beginners can use, we recommeneded sublime text \
+	editor, brackets, and notepad++. After, we than made a single web page while going \
+	over basics of html and css. Download the files below to go over what we made.',
+    links: [
+      {
+        name: 'Tutorial',
+        url: 'https://www.codecademy.com/learn/learn-html-css'
+      },
+      {
+        name: 'HTML',
+        url: 'http://www.w3schools.com/html/'
+      },
+	  {
+		name: 'CSS',
+		url: 'http://www.w3schools.com/css/'
+	  }
+    ],
+    files: [
+      'index.html',
+      'main.css',
+      'img.png',
+      'pic.png'
+    ]
+  }
+];
